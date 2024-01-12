7c90b736b696f2389ee4c7abe940fa8c8c1c53d8
Diogo Martins de Assis <diogomrtsa@gmail.com>
2024-01-12 17:59:15 -0300

    Método que ordena um slice com o método de ordenação por inserção

---
+++ OrdenacaoPorInsercao/ordenacao_por_insercao.go
@@ -0,0 +1,14 @@
+package ordenacaoporinsercao
+
+func Ordenar(arr []int) []int {
+	for j := 1; j < len(arr); j++ {
+		chave := arr[j]
+		i := j - 1
+		for i >= 0 && arr[i] > chave {
+			arr[i+1] = arr[i]
+			i--
+		}
+		arr[i+1] = chave
+	}
+	return arr
+}
