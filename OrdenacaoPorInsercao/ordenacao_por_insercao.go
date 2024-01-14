de3258bf6b2833faf53c284d8534d0a3ed4d2c51
Diogo Martins de Assis <diogomrtsa@gmail.com>
2024-01-14 13:34:26 -0300

    Criando método que ordena o array de forma decrescente

--- OrdenacaoPorInsercao/ordenacao_por_insercao.go
+++ OrdenacaoPorInsercao/ordenacao_por_insercao.go
@@ -12,3 +12,16 @@ func Ordenar(arr []int) []int {
 	}
 	return arr
 }
+
+func OrdenarDecrescente(arr []int) []int {
+	for j := 1; j < len(arr); j++ {
+		chave := arr[j]
+		i := j - 1
+		for i >= 0 && arr[i] < chave {
+			arr[i+1] = arr[i]
+			i--
+		}
+		arr[i+1] = chave
+	}
+	return arr
+}
