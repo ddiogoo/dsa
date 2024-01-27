726bfd12225c0eb46f1f4f58bf6073c9efbab25d
Diogo Martins de Assis <diogomrtsa@gmail.com>
2024-01-27 00:47:19 -0300

    Implementando método que junta duas listas

---
+++ ProjetoDeAlgoritmos/AbordagemDivisaoEConquista/abordagem.go
@@ -0,0 +1,29 @@
+func merge(A *[]int, p int, q int, r int) {
+	n1 := q - p + 1
+	n2 := r - q
+
+	L := make([]int, n1+1)
+	R := make([]int, n2+1)
+
+	for i := 1; i <= n1; i++ {
+		L[i] = (*A)[p+i-1]
+	}
+	for j := 1; j <= n2; j++ {
+		R[j] = (*A)[q+j]
+	}
+
+	L = append(L, int(^uint(0)>>1))
+	R = append(R, int(^uint(0)>>1))
+
+	i, j := 1, 1
+	var k int
+	for k = p; k <= r; k++ {
+		if L[i] <= R[j] {
+			(*A)[k] = L[i]
+			i = i + 1
+		} else {
+			(*A)[k] = R[j]
+			j = j + 1
+		}
+	}
+}
