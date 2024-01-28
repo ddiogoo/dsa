eb0a0116463aa1ae3c506ffa0093bb69dadcba99
Diogo Martins de Assis <diogomrtsa@gmail.com>
2024-01-28 14:22:17 -0300

    Atualizando entrypoint da nossa aplicação

--- main.go
+++ main.go
@@ -2,13 +2,10 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
-	"os"
-	"os/exec"
-	"runtime"
 
 	ordenacaoporinsercao "github.com/ddiogoo/algoritmos-teoria-e-pratica/OrdenacaoPorInsercao"
 	abordagemdivisaoeconquista "github.com/ddiogoo/algoritmos-teoria-e-pratica/ProjetoDeAlgoritmos/AbordagemDivisaoEConquista"
+	utils "github.com/ddiogoo/algoritmos-teoria-e-pratica/Utils"
 )
 
 func main() {
@@ -18,88 +15,12 @@ func main() {
 	fmt.Print("1) Ordenação por Inserção\n2) Abordagem Divisão e Conquista\n")
 	fmt.Scan(&method)
 
-	clearConsole()
+	utils.ClearConsole()
 
 	switch method {
 	case "1":
-		ordenacaoPorInsercao()
+		ordenacaoporinsercao.Executar()
 	case "2":
-		abordagemDivisaoEConquista()
+		abordagemdivisaoeconquista.Executar()
 	}
-}
-
-func ordenacaoPorInsercao() {
-	fmt.Println("Qual o tamanho do array que você deseja ordenar?")
-	fmt.Println("(Os valores serão inseridos automaticamente no array)")
-
-	var size int
-	fmt.Print("Inserir tamanho: ")
-	fmt.Scan(&size)
-
-	clearConsole()
-
-	arr := gerarArray(size)
-	fmt.Println("Antes de ordenar: ", arr)
-
-	arr = ordenacaoporinsercao.Ordenar(arr)
-	fmt.Println("Depois de ordenar: ", arr)
-
-	otherArr := []int{31, 41, 59, 26, 41, 58}
-	fmt.Println("Antes de ordenar: ", otherArr)
-
-	ordenacaoporinsercao.OrdenarDecrescente(otherArr)
-	fmt.Println("Depois de ordenar: ", otherArr)
-}
-
-func abordagemDivisaoEConquista() {
-	fmt.Println("Qual o tamanho do array que você deseja ordenar?")
-	fmt.Println("(Os valores serão inseridos automaticamente no array)")
-
-	var size int
-	fmt.Print("Inserir tamanho: ")
-	fmt.Scan(&size)
-
-	clearConsole()
-
-	arr := gerarArray(size)
-	fmt.Println("Antes de ordenar: ", arr)
-
-	abordagemdivisaoeconquista.MergeSort(&arr, 0, len(arr)-1)
-	fmt.Println("Depois de ordenar: ", arr)
-
-	otherArr := []int{31, 41, 59, 26, 41, 58}
-	fmt.Println("Antes de ordenar: ", otherArr)
-
-	abordagemdivisaoeconquista.MergeSort(&otherArr, 0, len(otherArr)-1)
-	fmt.Println("Depois de ordenar: ", otherArr)
-}
-
-func gerarArray(size int) []int {
-	arr := []int{}
-	r := rand.New(rand.NewSource(99))
-
-	s := size
-	for size > 0 {
-		arr = append(arr, r.Int()%s)
-		size--
-	}
-
-	return arr
-}
-
-func clearConsole() {
-	var cmd *exec.Cmd
-
-	switch runtime.GOOS {
-	case "linux", "darwin":
-		cmd = exec.Command("clear")
-	case "windows":
-		cmd = exec.Command("cmd", "/c", "cls")
-	default:
-		panic("Comando não suportado")
-		return
-	}
-
-	cmd.Stdout = os.Stdout
-	cmd.Run()
 }
