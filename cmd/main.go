3035895b4b64dc09999a3d71df2604e56b64c1fd
Diogo Martins de Assis <diogomrtsa@gmail.com>
2024-02-09 21:11:51 -0300

    Criando método principal para executar nossos algoritmos

---
+++ cmd/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+
+	divideandconquerapproach "github.com/ddiogoo/algorithms-theory-and-practice/DivideAndConquerApproach"
+	insertionsort "github.com/ddiogoo/algorithms-theory-and-practice/InsertionSort"
+	"github.com/ddiogoo/algorithms-theory-and-practice/utils"
+)
+
+func main() {
+	var method string
+	fmt.Print("Enter method: ")
+	fmt.Scan(&method)
+
+	utils.ClearConsole()
+	switch method {
+	case "1":
+		insertionsort.Run()
+	case "2":
+		divideandconquerapproach.Run()
+	}
+}
