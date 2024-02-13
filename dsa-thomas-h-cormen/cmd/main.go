f182e7b2c081dad32ce7f65c4eaea37dc74de5d6
ddiogoo <diogomrtsa@gmail.com>
2024-02-12 22:26:02 -0300

    Alterando repositório

---
+++ dsa-thomas-h-cormen/cmd/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+
+	divideandconquerapproach "github.com/ddiogoo/algorithms-theory-and-practice/tree/main/dsa-thomas-h-cormen/internal/DivideAndConquerApproach"
+	insertionsort "github.com/ddiogoo/algorithms-theory-and-practice/tree/main/dsa-thomas-h-cormen/internal/InsertionSort"
+	"github.com/ddiogoo/algorithms-theory-and-practice/tree/main/dsa-thomas-h-cormen/utils"
+)
+
+func main() {
+	var method string
+	fmt.Print("Enter method: ")
+	fmt.Scan(&method)
+
+	utils.ClearConsole()
+	switch method {
+	case "1":
+		insertionsort.Run()
+	case "2":
+		divideandconquerapproach.Run()
+	}
+}
--- go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/ddiogoo/algorithms-theory-and-practice
-
-go 1.21.6
