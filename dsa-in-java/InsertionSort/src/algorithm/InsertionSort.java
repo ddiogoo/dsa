885fe21bf6b82c76dbbfb0636734e0e2a40acd0e
Diogo Martins de Assis <diogomrtsa@gmail.com>
2024-02-12 22:53:49 -0300

    Implementando o insertion sort

---
+++ dsa-in-java/InsertionSort/src/algorithm/InsertionSort.java
@@ -0,0 +1,16 @@
+package algorithm;
+
+public class InsertionSort {
+	public static void sort(char[] data) {
+		int n = data.length;
+		for(int i = 1; i < n; i++) {
+			char curr = data[i];
+			int j = i;
+			while(j > 0 && data[j - 1] > curr) {
+				data[j] = data[j - 1];
+				j--;
+			}
+			data[j] = curr;
+		}
+	}
+}
