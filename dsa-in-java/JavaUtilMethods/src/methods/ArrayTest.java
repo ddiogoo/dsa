6fc165e6bc7979ac4de34a0c1bcd673d92f997f7
Diogo Martins de Assis <diogomrtsa@gmail.com>
2024-02-13 23:28:00 -0300

    Explicando meu código passo a passo

--- dsa-in-java/JavaUtilMethods/src/methods/ArrayTest.java
+++ dsa-in-java/JavaUtilMethods/src/methods/ArrayTest.java
@@ -6,16 +6,17 @@ import java.util.Random;
 public class ArrayTest {
 	public static void run() {
 		int[] data = new int[10];
-		Random rand = new Random();
-		rand.setSeed(System.currentTimeMillis());
+		Random rand = new Random();               // a pseudo-random number generator
+		rand.setSeed(System.currentTimeMillis()); // use current time as a seed
 		
+		// fill the data array with pseudo-random numbers from 0 to 99, inclusive
 		for(int i = 0; i < data.length; i++) {
-			data[i] = rand.nextInt(100);
+			data[i] = rand.nextInt(100);          // the next pseudo-random number
 		}
 		
-		int[] orig = Arrays.copyOf(data, data.length);
+		int[] orig = Arrays.copyOf(data, data.length); // make a copy of the data array
 		System.out.println("arrays equal before sort: " + Arrays.equals(data, orig));
-		Arrays.sort(data);
+		Arrays.sort(data);                             // sorting the data array (orig is unchanged)
 		
 		System.out.println("arrays equal after sort: " + Arrays.equals(data, orig));
 		System.out.println("orig = " + Arrays.toString(orig));
